6514416d67084904f1e6ae268a1f27cb4eb47289
JacobRajah <jacob.rajah@mail.utoronto.ca>
2020-12-27 19:57:20 -0500

    designing software page

--- src/components/pages/software-exp.js
+++ src/components/pages/software-exp.js
@@ -4,12 +4,33 @@ import './software-exp.css';
 function DropDown(props) {
     const [drop, setDrop] = React.useState(0);
     return (
-        <span className="drop-down"
-                onClick={() => {setDrop(1);}}
-                onAnimationEnd={() => {setDrop(0)}}
+        <div className="SW-comp">
+            <span className="drop-down"
+                onClick={() => {drop ? setDrop(0) : setDrop(1)}}
                 drop={drop}>
-            <p className="d1"></p><p className="d2"></p>
-        </span>
+                <p className="d1"></p><p className="d2"></p>
+            </span>
+            <div className="SW-data">
+                <h4 className="SW-Cat" onClick={() => {drop ? setDrop(0) : setDrop(1)}}>Web Development</h4>
+                {drop ? <ExpandData/>: null}
+            </div>
+        </div>
+    )
+}
+
+function ExpandData(props) {
+    return (
+        <div className="exp-data">
+            <div>Node.js</div>
+        </div>
+    );
+}
+
+function ExperienceBar(props) {
+    return (
+        <div>
+            {/* Progress bar here */}
+        </div>
     )
 }
 
