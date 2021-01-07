203014561e29b6e0aa32dd032eac447a7b678ea0
JacobRajah <jacob.rajah@mail.utoronto.ca>
2021-01-07 00:34:09 -0500

    small changes to sw and wk pages and images imported for project page

--- src/components/pages/Projects/projects.js
+++ src/components/pages/Projects/projects.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import './projects.css'
+import Wave from '../../../images/waves.png';
+import LyricFynder from '../../../images/lyricfynder.png'
+import RecipeReady from '../../../images/recipeready.JPG'
 class Projects extends Component {
+
+    constructor(props) {
+        super();
+        this.projects = [
+            {
+                img: LyricFynder,
+                title: "",
+                description: ""
+            },
+            {
+                img: RecipeReady,
+                title: "",
+                description: ""
+            },
+            {
+                img: Wave,
+                title: "",
+                description: ""
+            }
+        ]
+    }
+
     render() {
         return (
             <div>
                 <div className="panel-PRJ">
                     <h3 className="prj-heading">Projects</h3>
+                    {this.projects.map((e,i) => {
+                        return(<img src={e.img} width="300" height="200" alt="cover"></img>)
+                    })}
                 </div>
                 <span className="Next-PRJ" onClick={() => this.props.backOptions()}>
                     <p className="a1"></p><p className="a2"></p>
