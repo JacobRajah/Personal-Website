19740bbf0c780f43399690ea53b32557a026e5c5
JacobRajah <jacob.rajah@mail.utoronto.ca>
2021-01-09 02:47:44 -0500

    designed proj section

--- src/components/pages/Projects/projects.js
+++ src/components/pages/Projects/projects.js
@@ -3,6 +3,34 @@ import './projects.css'
 import Wave from '../../../images/waves.png';
 import LyricFynder from '../../../images/lyricfynder.png'
 import RecipeReady from '../../../images/recipeready.JPG'
+
+function Section(props) {
+    return (
+        <div className="proj-section">
+            <div className="proj-img">
+                <img src={props.img} width="300" height="200" alt="cover"></img>
+            </div>
+            <div className="proj-content">
+                <p>Insert Heading Here</p>
+                <p>Enter Software Used</p>
+                <p>Enter Description</p>
+            </div>
+            
+        </div>
+    )
+}
+
+
+// function ProjectComp(props) {
+//     return(
+//         <div>
+//             {this.projects.map((e,i) => {
+//                         return(<img src={e.img} width="300" height="200" alt="cover"></img>)
+//             })}
+//         </div>
+//     )
+// }
+
 class Projects extends Component {
 
     constructor(props) {
@@ -31,9 +59,7 @@ class Projects extends Component {
             <div>
                 <div className="panel-PRJ">
                     <h3 className="prj-heading">Projects</h3>
-                    {this.projects.map((e,i) => {
-                        return(<img src={e.img} width="300" height="200" alt="cover"></img>)
-                    })}
+                    <Section img={this.projects[0].img}></Section>
                 </div>
                 <span className="Next-PRJ" onClick={() => this.props.backOptions()}>
                     <p className="a1"></p><p className="a2"></p>
