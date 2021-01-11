eab8147e43423be85ec597bb43920c464e3b9168
JacobRajah <jacob.rajah@mail.utoronto.ca>
2021-01-11 03:46:18 -0500

    project plaintext page completed

--- src/components/pages/Projects/projects.js
+++ src/components/pages/Projects/projects.js
@@ -11,12 +11,12 @@ function Section(props) {
                 <img src={props.img} width="300" height="200" alt="cover"></img>
             </div>
             <div className="proj-content">
-                <h4>Insert Heading Here</h4>
+                <h4>{props.title}</h4>
                 <div className="sw-used">
                     <p className="sw-used-heading">Software Used:</p>
-                    <p className="sw-used-data">Enter Software Used</p>
+                    <p className="sw-used-data">{props.sw}</p>
                 </div>
-                <p className="proj-description">Enter Description</p>
+                <p className="proj-description">{props.description}</p>
             </div>
             
         </div>
@@ -29,7 +29,8 @@ function ProjectComp(props) {
         <div className="proj-data">
             {props.projects.map((e,i) => {
                         return(<Section img={e.img} 
-                                        title={e.title} 
+                                        title={e.title}
+                                        sw={e.sw} 
                                         description={e.description}></Section>)
             })}
         </div>
@@ -43,18 +44,29 @@ class Projects extends Component {
         this.projects = [
             {
                 img: LyricFynder,
-                title: "",
-                description: ""
+                title: "LyricFynder",
+                sw: "Node.js, React.js, MongoDB, Puppeteer, Genius/Spotify API, YouTube API",
+                description: "Full Stack Application which uses a user’s lyrical input" + 
+                            " along with the YouTube API and Puppeteer web scraping, "
+                            + "to determine the song related to the user’s lyrical input. "
+                            + "Offers other features such as Top charts of the week and trending music."
             },
             {
                 img: RecipeReady,
-                title: "",
-                description: ""
+                title: "Recipe Ready",
+                sw: "Java, Android Studio",
+                description: "Interactive app which allows users to streamline" +
+                " the process of deciding what meals to cook based on the amount" +
+                " of time they have. I worked on designing the UI and the functions" +
+                " for accessing recipe data"
             },
             {
                 img: Wave,
-                title: "",
-                description: ""
+                title: "Wave Simulator",
+                sw: "MatLab",
+                description: "Interactive UI that allows users to visualize the " +
+                "construction and desconstruction of sinusoidal waves with " + 
+                "features such as variable amplitude"
             }
         ]
     }
